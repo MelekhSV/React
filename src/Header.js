0c82b1b483dd91562c65755209fac4b26b16c67c
MelekhSV <melex.1998@mail.ru>
2021-11-20 13:20:25 +0300

    lesson 9

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header =() => {
+    return (
+        <div>
+            <div className='Header'>
+                <li> World</li>
+            </div>
+        </div>
+    );
+}
+
+export  default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+
+const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>Hello</li>
+                <li>Hi</li>
+            </ul>
+        </div>
+    );
+}
+export default Technologies;
